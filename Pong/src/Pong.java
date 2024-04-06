4753941368da970acdd530cc5946478d859fa0cb
NickQZ <nic.ejones25@gmail.com>
2024-04-06 13:36:38 +1300

    Created a Window for Pong that is resizeble and can be closed.

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -1,10 +1,24 @@
-
-public class Pong {
-
-	public static void main(String[] args) {
+import javax.swing.JFrame;
+ 
+public class Pong extends JFrame {
+	
+	private final static String WINDOW_TITLE = "Pong";
+	private final static int WINDOW_WIDTH = 800;
+	private final static int WINDOW_HEIGHT = 600;
+	
+	public Pong() {
 		
-		System.out.println("This is my Pong Program.");
-
+		setTitle(WINDOW_TITLE);
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		setResizable(false);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
 	}
-
+	
+	public static void main (String [] args) {
+		
+		new Pong();
+	}
+	
 }

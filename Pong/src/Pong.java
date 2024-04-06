91042252780cb33f1892fc0e0677d1eedc5cb572
NickQZ <nic.ejones25@gmail.com>
2024-04-07 11:04:35 +1200

    Added useful comments, A Timer & Update method and a Rendering method.

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -2,23 +2,33 @@ import javax.swing.JFrame;
  
 public class Pong extends JFrame {
 	
+	//Window Title
 	private final static String WINDOW_TITLE = "Pong";
+	//Window Width
 	private final static int WINDOW_WIDTH = 800;
+	//Window Height
 	private final static int WINDOW_HEIGHT = 600;
 	
 	public Pong() {
-		
+		//Display Window Title As "Pong"
 		setTitle(WINDOW_TITLE);
+		//Display Window Width: 800 & Window Height: 600
 		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		//Is Window Resizable?
 		setResizable(false);
+		//Import Code from PongPanel
 		add(new PongPanel());
+		//Is Window Visible?
 		setVisible(true);
+		//On Exit Terminate Program
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 	}
 	
+	//When Program is Run...
 	public static void main(String[] args) {      
-		 
+		
+		//Run PongPanel.java 
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
  
           public void run() {
--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -4,15 +4,27 @@
    import java.awt.event.KeyEvent;
    import java.awt.event.KeyListener;
    import javax.swing.JPanel;
+   import javax.swing.Timer;
+   import java.awt.Graphics;
+   import java.awt.Graphics2D;
 
    public class PongPanel extends JPanel implements ActionListener, KeyListener {
-
+	   
+	   //Set Background of JPanel Black
 	   private final static Color BACKGROUND_COLOUR = Color.BLACK;
+	   //Import Timer
+	   private final static int TIMER_DELAY = 5;
 
 	   public PongPanel() {
-
+		   
+		   //Set Background to Background_Colour
 	       setBackground(BACKGROUND_COLOUR);
-
+	       
+	       //Set Timer start
+	       Timer timer = new Timer(TIMER_DELAY, this);
+	       timer.start();
+	       
+	       update();
 	   }
 
       @Override
@@ -34,6 +46,17 @@
       public void actionPerformed(ActionEvent event) {
 
       }
+      
+      public void update() {
+    	  
+      }
+      //Draws graphics on JPanel
+      @Override
+      public void paintComponent(Graphics g) {
+          super.paintComponent(g);
+          g.setColor(Color.WHITE);
+          g.fillRect(20, 20, 100, 100);
+       }
    }
 
   

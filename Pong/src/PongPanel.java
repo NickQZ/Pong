dbe5389db733b0490336bd0c23965989bd6e478d
NickQZ <nic.ejones25@gmail.com>
2024-04-07 19:09:35 +1200

    Main Code Without Interaction This is the Source Code with all of the basic values and variables(Not including, A Score Counter and Actual Controls)
    V.2.1

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -1,118 +1,134 @@
-   import java.awt.Color;
-   import java.awt.event.ActionEvent;
-   import java.awt.event.ActionListener;
-   import java.awt.event.KeyEvent;
-   import java.awt.event.KeyListener;
-   import javax.swing.JPanel;
-   import javax.swing.Timer;
-   import java.awt.Graphics;
-   import java.awt.Graphics2D;
-   import java.awt.Stroke;
-   import java.awt.BasicStroke;
-   import java.awt.BasicStroke;
+ import java.awt.BasicStroke;
+ import java.awt.Color;
+ import java.awt.Graphics;
+ import java.awt.Graphics2D;
+ import java.awt.Stroke;
+ import java.awt.event.ActionEvent;
+ import java.awt.event.ActionListener;
+ import java.awt.event.KeyEvent;
+ import java.awt.event.KeyListener;
+ import javax.swing.JPanel;
+ import javax.swing.Timer;  
 
-   public class PongPanel extends JPanel implements ActionListener, KeyListener { // Open PongPanel Class
-	   
-	   //Set Background of JPanel Black
-	   private final static Color BACKGROUND_COLOUR = Color.BLACK;
-	   //Import Timer
-	   private final static int TIMER_DELAY = 5;
+  public class PongPanel extends JPanel implements ActionListener, KeyListener {
+      
+       private final static Color BACKGROUND_COLOUR = Color.BLACK;
 
-	   public PongPanel() { // Open PongPanel
-		   
-		   //Set Background to Background_Colour
-	       setBackground(BACKGROUND_COLOUR);
-	       
-	       //Set Timer start
-	       Timer timer = new Timer(TIMER_DELAY, this);
-	       timer.start();
- 
-	   } // Close PongPanel
-	   
-      //Draws Centered Dotted Line
-      private void paintDottedLine(Graphics g) { // Open PaintDottedLine
-    	        Graphics2D g2d = (Graphics2D) g.create();
-    	        Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
-    	        g2d.setStroke(dashed);
-    	        g2d.setPaint(Color.WHITE);
-    	        g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
-    	        g2d.dispose();
-    	        
-       } // Close PaintDottedLine
+       private final static int TIMER_DELAY = 5;
       
-      private void paintSprite(Graphics g, Sprite sprite) {
-    	     g.setColor(sprite.getColour());
-    	     g.fillRect(sprite.getxPosition(), sprite.getyPosition(), sprite.getwidth(), sprite.getheight());
-    	}
-      
-      
-      @Override
-      public void keyPressed(KeyEvent event) { // Open KeyPressed
+       GameState gameState = GameState.INITIALISING;
+       
+       Ball ball;
 
-      } // Close KeyPressed
+       Paddle paddle1, paddle2;
 
-      @Override
-      public void keyReleased(KeyEvent event) { // Open KeyReleased
+       
+       public PongPanel() {
 
-      } // Close KeyReleased
+           setBackground(BACKGROUND_COLOUR);
 
-      @Override
-      public void keyTyped(KeyEvent event) { // Open keyTyped
+           Timer timer = new Timer(TIMER_DELAY,this);
 
-      } // Close keyTyped
-      
-      // Create Ball Object
-        GameState gameState = GameState.INITIALISING;
-        Ball ball;
-              
-        public void createObjects() {
-               ball = new Ball(getWidth(), getHeight());
-        }
-        
-        // Update Objects
-        private void update() {
-         switch(gameState) {
-         case INITIALISING: {
-         createObjects();
-         gameState = GameState.PLAYING;
-          break;
-         }
-          case PLAYING: {
-          break;
-        	            }
-          case GAMEOVER: {
-          break;
-          }
-         }
-             
-      } // Close Update
-      
-      
-      @Override
-      public void actionPerformed(ActionEvent event) { // Open ActionPreformed
-    	  update();
-    	  repaint();
+           timer.start();
 
-      } // Close ActionPreformed
-      
-      //Draws graphics on JPanel
-      @Override
-       public void paintComponent(Graphics g) {// Open PaintComponent
-    	  // Paint Dotted Line
-            super.paintComponent(g);
-            paintDottedLine(g);
-            // Paint Ball Sprite
-            if(gameState != GameState.INITIALISING) {
-                paintSprite(g, ball);
-            }
-       }// Close PaintComponent
-      
-      public void gameState(ActionEvent event){
-    	  
-      }
-     
-      
-      
-   } // Close PongPanel Class
+       }      
 
-  
+       public void createObjects() {
+
+           ball = new Ball(getWidth(), getHeight());
+
+           paddle1 = new Paddle(Player.One, getWidth(), getHeight());
+
+           paddle2 = new Paddle(Player.Two, getWidth(), getHeight());
+
+       }
+
+       private void update() {
+
+           switch(gameState) {
+
+               case INITIALISING: {
+
+                   createObjects();
+
+                   gameState = GameState.PLAYING;
+
+                   break;
+
+               }
+
+               case PLAYING: {
+
+                   break;
+
+               }
+
+               case GAMEOVER: {
+
+                   break;
+
+               }
+
+           }
+
+       }
+       
+       private void paintDottedLine(Graphics g) {
+          Graphics2D g2d = (Graphics2D) g.create();
+              Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+              g2d.setStroke(dashed);
+              g2d.setPaint(Color.WHITE);
+              g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+              g2d.dispose();
+       }
+       
+       private void paintSprite(Graphics g, Sprite sprite) {
+           g.setColor(sprite.getColour());
+           g.fillRect(sprite.getxPosition(), sprite.getyPosition(), sprite.getwidth(), sprite.getheight());
+
+       }
+
+       @Override
+       public void keyPressed(KeyEvent event) {       
+       }
+
+       @Override
+
+       public void keyReleased(KeyEvent event) {    	   
+       }
+
+       @Override
+
+       public void keyTyped(KeyEvent event) {
+       }
+
+       @Override
+
+       public void actionPerformed(ActionEvent event) {
+
+           update();
+
+           repaint();
+
+       }
+
+       @Override
+       public void paintComponent(Graphics g) {
+
+           super.paintComponent(g);
+
+           paintDottedLine(g);
+
+           if(gameState != GameState.INITIALISING) {
+
+               paintSprite(g, ball);
+
+               paintSprite(g, paddle1);
+
+               paintSprite(g, paddle2);
+
+           }
+
+       }
+
+  }
